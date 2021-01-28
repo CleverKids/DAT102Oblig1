d209a999d023d8cc14dd72a19c1bcf382d77be8c
Eivind <69585726+594620@users.noreply.github.com>
2021-01-28 13:11:01 +0100

    Create Sjanger.java

---
+++ src/oving2/film/Sjanger.java
@@ -0,0 +1,17 @@
+package oving2.film;
+
+public enum Sjanger {
+	Action, Drama, Komedie, Fantasi, Scifi;
+
+	public static Sjanger finnSjanger(String navn) {
+		Sjanger sjang = null;
+		for (Sjanger sj : Sjanger.values()) {
+			if (sj.toString().equals(navn.toUpperCase())) {
+				sjang = sj;
+				break;
+			}
+		}
+		return sjang;
+	}
+
+}
