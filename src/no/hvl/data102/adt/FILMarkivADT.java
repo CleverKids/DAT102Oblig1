640aad66400f4b0df1b118d1f41561a13f5f63f5
Eivind <69585726+594620@users.noreply.github.com>
2021-02-11 13:43:19 +0100

    Create FILMarkivADT.java

---
+++ src/no/hvl/data102/adt/FILMarkivADT.java
@@ -0,0 +1,58 @@
+package no.hvl.data102.adt;
+
+import no.hvl.data102.Film;
+import no.hvl.data102.Sjanger;
+
+public interface FILMarkivADT {
+
+	/**
+	 * Henter alle filmer i arkivet
+	 * @return film tabellen
+	 */
+	Film[] hentFilmTabell();
+	
+	//søke etter film ved hjelp av filmnr.
+	Film sokFilmNr(int soknr);
+	
+	/**
+	 *legger til en ny film i arkivet
+	 *@param nyfilm
+	 */
+	void leggTilFilm(Film nyFilm);
+	
+	/**
+	 * Sletter film om den eksisterer
+	 * @param filmnr
+	 * @return ble filmen slettet
+	 */
+	boolean slettFilm(int filmnr);
+	
+	/**
+	 * søk etter film ved bruk av tittel delstreng
+	 * @param delstreng
+	 * @return filmer med gitt delstreng
+	 */
+	Film[] soekTittel(String delstreng);
+	
+	/**
+	 * søke etter filmer med produsent delstreng
+	 * @param delstreng
+	 * @return filmer fra produsenten
+	 */
+	Film[] soekProdusent(String delstreng);
+	
+	/**
+	 * viser antall sjangere i arkivet
+	 * @param Sjanger
+	 * @return antall filmer
+	 */
+	int antall (Sjanger Sjanger);
+
+	/**
+	 * viser antall filmer i arkivet
+	 * @return antall filmer
+	 */
+	int antall ();
+
+}//interface
+
